31551d5df84280d1e935c5883545c6ffab7411f2
pdavidson1997 <david.siraly01@gmail.com>
2019-05-14 17:40:15 +0200

    database work

--- src/main/java/game/FXMLController.java
+++ src/main/java/game/FXMLController.java
@@ -1,23 +1,32 @@
 package game;
 
+import database.gamer.Gamer;
+import database.jpa.DBTools;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 
 import javafx.scene.image.Image;
 
+import javafx.scene.control.TableView;
 import java.awt.event.MouseEvent;
 
 public class FXMLController {
     
     @FXML
     private Button B_start;
+    @FXML
+    private Button winButton;
     @FXML
     private ImageView b00;
     @FXML
@@ -39,11 +48,15 @@ public class FXMLController {
     @FXML
     private Button B_gamebegin;
     @FXML
+    private Button leadbButton;
+    @FXML
     private AnchorPane start;
     @FXML
     private AnchorPane gamepane;
     @FXML
     private AnchorPane preppane;
+    @FXML
+    private AnchorPane leaderboardPane;
     @FXML
     private TextField gamer1;
     @FXML
@@ -52,6 +65,8 @@ public class FXMLController {
     private Label messageout;
     @FXML
     private Label stepLabel;
+    @FXML
+    private TableView leaderboard;
 
 
     public static ImageView[][] btns = new ImageView[3][3];
@@ -69,6 +84,9 @@ public class FXMLController {
     private int[] FirstClickNumber = new int[2];
     private int StepCounter=0;
 
+    private Gamer gamer = new Gamer();
+    DBTools conn = new DBTools();
+
     private ActionEvent event;
 
     @FXML
@@ -80,7 +98,27 @@ public class FXMLController {
     private void B_gamebeginClick(ActionEvent event) {
         preppane.setVisible(false);
         gamepane.setVisible(true);
+
+        gamer.setName(gamer1.getText());
+
+        conn.addGamer(gamer);
+
         gamebegin();
+    }
+    @FXML
+    private void winButtonClick(ActionEvent event) {
+        start.setVisible(true);
+        gamepane.setVisible(false);
+        conn.updateGamer(gamer);
+        resetGame();
+    }
+    @FXML
+    private void leadbShow(ActionEvent event) {
+        /*ObservableList<Gamer> top = FXCollections.observableList(conn.getleaderboard());
+
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        winscore.setCellValueFactory(new PropertyValueFactory<>("score"));
+        leaderboard.setItems(top);*/
     }
 
     @FXML
@@ -135,12 +173,7 @@ public class FXMLController {
         btns[2][1] = b21;
         btns[2][2] = b22;
 
-        for(byte i=0;i<3;i++)
-            ground[0][i] = 1;
-        for(byte i=0;i<3;i++)
-            ground[1][i] = 0;
-        for(byte i=0;i<3;i++)
-            ground[2][i] = 2;
+        resetGame();
 
         blackblack = new Image(getClass().getResource("blackblack.png").toString());
         whiteblack = new Image(getClass().getResource("whiteblack.png").toString());
@@ -157,6 +190,20 @@ public class FXMLController {
     {
         player1=gamer1.getText();
         nameLabel.setText(player1);
+    }
+
+    private void resetGame()
+    {
+        for(byte i=0;i<3;i++)
+            ground[0][i] = 1;
+        for(byte i=0;i<3;i++)
+            ground[1][i] = 0;
+        for(byte i=0;i<3;i++)
+            ground[2][i] = 2;
+
+        winButton.setVisible(false);
+        StepCounter = 0;
+        paint();
     }
 
     private static void paint()
@@ -270,14 +317,12 @@ public class FXMLController {
                 goodness++;
         }
         messageout.setText(goodness + " ");
-        System.out.println(ground[0][0] + " " + ground[0][1] + " " + ground[0][2]);
-        System.out.println(ground[1][0] + " " + ground[1][1] + " " + ground[1][2]);
-        System.out.println(ground[2][0] + " " + ground[2][1] + " " + ground[2][2]);
         if(goodness==6)
             playwin();
     }
     public void playwin()
     {
         messageout.setText("Game win.");
+        winButton.setVisible(true);
     }
 }
