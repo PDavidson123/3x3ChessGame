b90d4bda48c880a142f480d15b29120a163df0a3
pdavidson1997 <david.siraly01@gmail.com>
2019-05-14 11:17:05 +0200

    Repushed

---
+++ src/main/java/game/FXMLController.java
@@ -0,0 +1,283 @@
+package game;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+
+import javafx.scene.image.Image;
+
+import java.awt.event.MouseEvent;
+
+public class FXMLController {
+    
+    @FXML
+    private Button B_start;
+    @FXML
+    private ImageView b00;
+    @FXML
+    private ImageView b01;
+    @FXML
+    private ImageView b02;
+    @FXML
+    private  ImageView b10;
+    @FXML
+    private  ImageView b11;
+    @FXML
+    private ImageView b12;
+    @FXML
+    private ImageView b20;
+    @FXML
+    private ImageView b21;
+    @FXML
+    private ImageView b22;
+    @FXML
+    private Button B_gamebegin;
+    @FXML
+    private AnchorPane start;
+    @FXML
+    private AnchorPane gamepane;
+    @FXML
+    private AnchorPane preppane;
+    @FXML
+    private TextField gamer1;
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private Label messageout;
+    @FXML
+    private Label stepLabel;
+
+
+    public static ImageView[][] btns = new ImageView[3][3];
+    private static byte[][] ground = new byte[3][3];
+    private static String player1;
+    private static Image blackblack;
+    private static Image whiteblack;
+    private static Image whitewhite;
+    private static Image blackwhite;
+    private static Image black;
+    private static Image white;
+
+    private boolean IsFirstClick = true;
+    private boolean ColorSelection = true;
+    private int[] FirstClickNumber = new int[2];
+    private int StepCounter=0;
+
+    private ActionEvent event;
+
+    @FXML
+    private void B_startClick(ActionEvent event) {
+        preppane.setVisible(true);
+        start.setVisible(false);
+    }
+    @FXML
+    private void B_gamebeginClick(ActionEvent event) {
+        preppane.setVisible(false);
+        gamepane.setVisible(true);
+        gamebegin();
+    }
+
+    @FXML
+    private void b00Click() { step(0,0); }
+    @FXML
+    private void b01Click() { step(0,1); }
+    @FXML
+    private void b02Click() { step(0,2); }
+    @FXML
+    private void b10Click() { step(1,0); }
+    @FXML
+    private void b11Click() { step(1,1); }
+    @FXML
+    private void b12Click() { step(1,2); }
+    @FXML
+    private void b20Click() { step(2,0); }
+    @FXML
+    private void b21Click() { step(2,1); }
+    @FXML
+    private void b22Click() { step(2,2); }
+
+    public void initialize() {
+        start.setVisible(true);
+        gamepane.setVisible(false);
+        preppane.setVisible(false);
+
+        /*for(int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                try
+                {
+                    Field f = this.getClass().getDeclaredField("b" + Integer.toString(i) + Integer.toString(j));
+                    btns[i][j]=(Button)f.get(this);
+                }
+                catch (NoSuchFieldException ex)
+                {
+                    //System.out.println("no such field");
+                }
+                catch (IllegalAccessException ex2)
+                {
+                    //System.out.println("illegal access");
+                }
+            }
+        }*/
+
+        btns[0][0] = b00;
+        btns[0][1] = b01;
+        btns[0][2] = b02;
+        btns[1][0] = b10;
+        btns[1][1] = b11;
+        btns[1][2] = b12;
+        btns[2][0] = b20;
+        btns[2][1] = b21;
+        btns[2][2] = b22;
+
+        for(byte i=0;i<3;i++)
+            ground[0][i] = 1;
+        for(byte i=0;i<3;i++)
+            ground[1][i] = 0;
+        for(byte i=0;i<3;i++)
+            ground[2][i] = 2;
+
+        blackblack = new Image(getClass().getResource("blackblack.png").toString());
+        whiteblack = new Image(getClass().getResource("whiteblack.png").toString());
+        whitewhite = new Image(getClass().getResource("whitewhite.png").toString());
+        blackwhite = new Image(getClass().getResource("blackwhite.png").toString());
+        black = new Image(getClass().getResource("black.png").toString());
+        white = new Image(getClass().getResource("white.png").toString());
+
+        stepLabel.setText(StepCounter + " . lépés");
+        paint();
+    }
+
+    public void gamebegin()
+    {
+        player1=gamer1.getText();
+        nameLabel.setText(player1);
+    }
+
+    private static void paint()
+    {
+        for(int i=0;i<3;i++)
+            for(int j=0;j<3;j++)
+            {
+                if (i==0 && j==0 || i== 0 && j==2 || i==1 && j==1 || i==2 && j==0 || i==2 && j==2 /*i == j || abs(i + j) == 2*/)
+                {
+                    switch (ground[i][j])
+                    {
+                        case 0:
+                            btns[i][j].setImage(black);
+                            break;
+                        case 1:
+                            btns[i][j].setImage(blackwhite);
+                            break;
+                        case 2:
+                            btns[i][j].setImage(blackblack);
+                    }
+                }
+                else
+                {
+                    switch (ground[i][j]) {
+                        case 0:
+                            btns[i][j].setImage(white);
+                            break;
+                        case 1:
+                            btns[i][j].setImage(whitewhite);
+                            break;
+                        case 2:
+                            btns[i][j].setImage(whiteblack);
+                    }
+                }
+            }
+    }
+
+    public void step(int a,int b)
+    {
+        if(IsFirstClick)
+        {
+            if(ground[a][b] == 0)
+                messageout.setText("Rossz választás.");
+            else
+            {
+                if(ColorSelection && ground[a][b]==2 || !ColorSelection && ground[a][b]==1)
+                    messageout.setText("Rossz színt választottál.");
+                else if(ColorSelection && ground[a][b]==1 || !ColorSelection && ground[a][b]==2)
+                {
+                    FirstClickNumber[0] = a;
+                    FirstClickNumber[1] = b;
+                    IsFirstClick = false;
+                    messageout.setText("Kiválasztva. -->" + FirstClickNumber[0] + " " + FirstClickNumber[1]);
+                }
+            }
+        }
+        else
+        {
+            if (a==FirstClickNumber[0] && b==FirstClickNumber[1])
+                messageout.setText("Ugyan oda nem léphetsz.");
+            else
+            {
+                if(ground[a][b]==0 && ground[FirstClickNumber[0]][FirstClickNumber[1]]==1 && ColorSelection)
+                {
+                    if(FirstClickNumber[0]<a)
+                    {
+                        if(ground[a][b]==0)
+                        {
+                            ground[FirstClickNumber[0]][FirstClickNumber[1]] =0;
+                            ground[a][b]=1;
+
+                            paint();
+                            IsFirstClick=true;
+                            ColorSelection=false;
+                        }
+                    }
+                    else
+                        messageout.setText("Vissza nem léphetsz.");
+                }
+                else if(ground[a][b]==0 && ground[FirstClickNumber[0]][FirstClickNumber[1]]==2 && !ColorSelection)
+                {
+                    if(FirstClickNumber[0]>a)
+                    {
+                        if(ground[a][b]==0)
+                        {
+                            ground[FirstClickNumber[0]][FirstClickNumber[1]] =0;
+                            ground[a][b]=2;
+
+                            paint();
+                            IsFirstClick=true;
+                            ColorSelection=true;
+                        }
+
+                    }
+                }
+            }
+            StepCounter++;
+            stepLabel.setText(StepCounter + " . lépés");
+            chechwin();
+        }
+    }
+
+    public void chechwin()
+    {
+        int goodness=0;
+        for (int i=0;i<3;i++)
+        {
+            if(ground[0][i]==2)
+                goodness++;
+            if(ground[2][i]==1)
+                goodness++;
+        }
+        messageout.setText(goodness + " ");
+        System.out.println(ground[0][0] + " " + ground[0][1] + " " + ground[0][2]);
+        System.out.println(ground[1][0] + " " + ground[1][1] + " " + ground[1][2]);
+        System.out.println(ground[2][0] + " " + ground[2][1] + " " + ground[2][2]);
+        if(goodness==6)
+            playwin();
+    }
+    public void playwin()
+    {
+        messageout.setText("Game win.");
+    }
+}
